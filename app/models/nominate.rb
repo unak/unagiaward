2fc47242cd0d5ff17b9d2faf9a7679427f22da40
U.Nakamura <usa@garbagecollect.jp>
2014-04-17 19:26:14 +0900

    validate length.

--- app/models/nominate.rb
+++ app/models/nominate.rb
@@ -2,6 +2,6 @@ class Nominate < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true
-  validates :candidate, presence: true
-  validates :reason, presence: true
+  validates :candidate, presence: true, length: { maximum: 32 }
+  validates :reason, presence: true, length: { maximum: 140 }
 end
