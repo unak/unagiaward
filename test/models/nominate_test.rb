af516bf830d4b8f5f06df67f21ed06913b903e09
U.Nakamura <usa@garbagecollect.jp>
2014-04-17 19:26:32 +0900

    added model tests.

--- test/models/nominate_test.rb
+++ test/models/nominate_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class NominateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validations" do
+    nominate = Nominate.new
+    refute nominate.valid?
+    assert nominate.errors[:user].any?
+    assert nominate.errors[:candidate].any?
+    assert nominate.errors[:reason].any?
+
+    nominate.user = users(:one)
+    refute nominate.valid?
+    refute nominate.errors[:user].any?
+    assert nominate.errors[:candidate].any?
+    assert nominate.errors[:reason].any?
+
+    nominate.candidate = ""
+    refute nominate.valid?
+    assert nominate.errors[:candidate].any?
+    assert nominate.errors[:reason].any?
+
+    nominate.candidate = "a" * 33
+    refute nominate.valid?
+    assert nominate.errors[:candidate].any?
+    assert nominate.errors[:reason].any?
+
+    nominate.candidate = "a" * 32
+    refute nominate.valid?
+    refute nominate.errors[:candidate].any?
+    assert nominate.errors[:reason].any?
+
+    nominate.reason = ""
+    refute nominate.valid?
+    assert nominate.errors[:reason].any?
+
+    nominate.reason = "a" * 141
+    refute nominate.valid?
+    assert nominate.errors[:reason].any?
+
+    nominate.reason = "a" * 140
+    assert nominate.valid?
+    refute nominate.errors[:reason].any?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,81 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validations" do
+    user = User.new
+    refute user.valid?
+    assert user.errors[:provider].any?
+    assert user.errors[:uid].any?
+    assert user.errors[:name].any?
+
+    user.provider = ""
+    refute user.valid?
+    assert user.errors[:provider].any?
+    assert user.errors[:uid].any?
+    assert user.errors[:name].any?
+
+    user.provider = "twitter"
+    refute user.valid?
+    refute user.errors[:provider].any?
+    assert user.errors[:uid].any?
+    assert user.errors[:name].any?
+
+    user.uid = ""
+    refute user.valid?
+    assert user.errors[:uid].any?
+    assert user.errors[:name].any?
+
+    user.uid = "999"
+    refute user.valid?
+    refute user.errors[:uid].any?
+    assert user.errors[:name].any?
+
+    user.name = ""
+    refute user.valid?
+    assert user.errors[:name].any?
+
+    user.name = "foo"
+    assert user.valid?
+    refute user.errors[:name].any?
+
+    user.provider = users(:one).provider
+    user.uid = users(:one).uid
+    refute user.valid?
+    assert user.errors[:uid].any?
+
+    user.provider = users(:one).provider
+    user.uid = "999"
+    assert user.valid?
+    refute user.errors[:uid].any?
+
+    user.provider = "github"
+    user.uid = users(:one).uid
+    assert user.valid?
+    refute user.errors[:uid].any?
+  end
+
+  test "create_with_auth" do
+    user = nil
+
+    assert_difference("User.count") do
+      user = User.create_with_auth("provider" => "twitter", "uid" => "999", "info" => { "nickname" => "nick" })
+    end
+    assert_equal "twitter", user.provider
+    assert_equal "999", user.uid
+    assert_equal "nick", user.name
+
+    assert_difference("User.count") do
+      user = User.create_with_auth("provider" => "twitter", "uid" => "998", "info" => { "name" => "name" })
+    end
+    assert_equal "twitter", user.provider
+    assert_equal "998", user.uid
+    assert_equal "name", user.name
+
+    assert_difference("User.count") do
+      user = User.create_with_auth("provider" => "twitter", "uid" => "997", "info" => { "nickname" => "nick", "name" => "name" })
+    end
+    assert_equal "twitter", user.provider
+    assert_equal "997", user.uid
+    assert_equal "nick", user.name
+  end
 end
